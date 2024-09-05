b169ba4c7aa881f84b144009bf292e33a6316968
Kitrick Bell <kitrickd.bell@gmail.com>
2024-09-05 10:08:56 -0500

    starting spring boot

---
+++ weather-client-app/src/main/java/com/example/weather_client_app/WeatherClientAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather_client_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherClientAppApplication.class, args);
+	}
+
+}
---
+++ weather-client-app/src/test/java/com/example/weather_client_app/WeatherClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weather_client_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
